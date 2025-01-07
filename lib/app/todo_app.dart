02b98f63adb99f97e7dfe94e506c082c87a1499e
farzanamohammadi71 <farzanamohammadi71@gmail.com>
2025-01-07 18:39:28 +0430

    The theme of app is wrote.

--- lib/app/todo_app.dart
+++ lib/app/todo_app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_list/screens/home_screen.dart';
+
+import '../config/theme/app_theme.dart';
 
 class TodoApp extends StatelessWidget {
   const TodoApp({super.key});
@@ -7,10 +10,10 @@ class TodoApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(),
-      ),
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      home: HomeScreen(),
     );
   }
 }
